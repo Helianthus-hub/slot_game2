6532faed9969fcadd5508ab030f69e42c2e312ac
antoxaMc <gray92929292@gmail.com>
2026-03-20 13:45:13 +0100

    Full Heart grid

---
+++ nodes/Heart.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+
+public partial class Heart : Symbol
+{
+	public override string ImagePath => "res://sprites/heart.png";
+}
--- nodes/SlotMachineEngine.cs
+++ nodes/SlotMachineEngine.cs
@@ -3,25 +3,36 @@ using System;
 
 public partial class SlotMachineEngine : Node2D
 {
-	[Export] private GridContainer _grid;
+	private TextureRect[] GridSlots = new TextureRect[25];
+	private Symbol[] symbols = new Symbol[25];
+	
+	
+	[Export] private GridContainer Grid;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		foreach (Node child in _grid.GetChildren()){
-		child.Free();}
-		
-		//for (int i = 0; i < 5; i++)
-		//{
-		   //for (int j = 0; j < 5; j++)
-			//{
-				String path = "res://sprites/sprite.png";
+		InitializeGrid();
 				Sprite2D mySprite = new Sprite2D();
 				Clover cl = new Clover();
+				Heart he = new Heart();
 				Texture2D myTexture = GD.Load<Texture2D>(cl.ImagePath);
 				mySprite.Texture = myTexture;
-				var textureRect = new TextureRect();
-				textureRect.Texture = GD.Load<Texture2D>(cl.ImagePath);
-				_grid.AddChild(textureRect);
+				//var textureRect = new TextureRect();
+				//textureRect.Texture = GD.Load<Texture2D>(cl.ImagePath);
+				//var textureRect2 = new TextureRect();
+				//Grid.AddChild(textureRect);
+				//GridSlots[0] = textureRect;
+				//textureRect2.Texture = GD.Load<Texture2D>(he.ImagePath);
+				//GridSlots[1] = textureRect2;
+				for(int i = 0; i < 25; i++){
+					var textureRect = new TextureRect();
+					textureRect.Texture = GD.Load<Texture2D>(he.ImagePath);
+					GridSlots[i] = textureRect;
+				}
+				BuildGrid(GridSlots);
+				
+				
+				
 				
 			//}
 		//}
@@ -32,9 +43,14 @@ public partial class SlotMachineEngine : Node2D
 	{
 		
 	}
-	
-	public void SetSymbolAt(int index, Clover symbol)
-{
-	
-}
+	public void InitializeGrid(){
+		Grid.Columns = 5;
+		foreach (Node child in Grid.GetChildren()){
+		child.Free();}
+	}
+	public void BuildGrid(params TextureRect[] textures){
+		foreach(var texture in textures){
+			Grid.AddChild(texture);
+		}
+	}
 }
