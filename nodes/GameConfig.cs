aa892e09a559a69eab7eb74f0a4fcc90bb741537
Helianthus-hub <duckspot.yt@gmail.com>
2026-03-20 19:03:55 +0100

    pridejau nebaigta 3 of a kind funkcija padariau kad gridas butu kaip 2d array nes kitaip labai sunku interpretuoti elementu pozicijas

---
+++ nodes/GameConfig.cs
@@ -0,0 +1,5 @@
+//Cia rasom visus Final timpo variables kad galetume
+//naudoti savo programoje ir reusinti keisti tik sitoje vietoje
+public static class GameConfig{
+public static readonly int ThreeOfAKindPayout = 100;
+}
--- nodes/SlotMachineEngine.cs
+++ nodes/SlotMachineEngine.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 
 public partial class SlotMachineEngine : Node2D
 {
-	private TextureRect[] GridSlots = new TextureRect[25];
-	private Symbol[] Symbols = new Symbol[25];
+	private TextureRect[,] GridSlots = new TextureRect[5, 5];
+	private Symbol[,] Symbols = new Symbol[5, 5];
 	
 	
 	[Export] private GridContainer Grid;
@@ -14,6 +14,9 @@ public partial class SlotMachineEngine : Node2D
 	{
 			InitializeGrid();
 			BuildGrid();
+			GD.Print("hello");
+			CalculateThreeOfAKind();
+
 					//Sprite2D mySprite = new Sprite2D();
 					/*over cl = new Clover();
 					Heart he = new Heart();
@@ -49,6 +52,7 @@ public partial class SlotMachineEngine : Node2D
 		
 		
 	}
+	
 	//Pabandysiu padarti kad random simbolius sudetu i grida
 	//Saugom elementus mape su ju paths to sprite
 	Dictionary<int, string> SymbolPaths = new Dictionary<int, string>() 
@@ -64,38 +68,58 @@ public partial class SlotMachineEngine : Node2D
 			child.Free();//istrina is atminties kad duplicates nebutu
 		}
 		Random rng = new Random();
-		for(int i = 0; i < 25; i++){
-			int randomPick = rng.Next(0, SymbolPaths.Count);
-			Symbols[i] = randomPick switch {
-				0 => new Heart(),
-				1 => new Clover(),
-				2 => new Ankh(),
-				3 => new Pentagram(),
-				_ => new Heart()
-			};
-		}	
+		for(int row = 0; row < 5; row++){
+			for(int col = 0; col < 5; col++){
+				int randomPick = rng.Next(0, SymbolPaths.Count);
+				Symbols[row, col] = randomPick switch {
+					0 => new Heart(),
+					1 => new Clover(),
+					2 => new Ankh(),
+					3 => new Pentagram(),
+					_ => new Heart()
+				};
+			}
+		}
 	}
 	// Called every frame. 'delta' is the elapsed time sincse the previous frame.
 	public override void _Process(double delta)
 	{
 		
 	}
-	public void BuildGrid(){
-	
-		for(int i = 0; i < 25; i++){
-			var textureRect = new TextureRect();
-			textureRect.Texture = GD.Load<Texture2D>(Symbols[i].ImagePath);
-			GridSlots[i] = textureRect;
+	//Cia bus pagrindine logika kaip skaiciuojami laimejimai veliau bus kiti skirtingi laimejimo variantai dabar noriu padaryti 3 of a kind
+	//kinda works tik reik pagalvot kaip antra for loop padaryti kad nebutu out of
+	//bounds exception, dabar tikrina 3 simbolius is eiles bet reikia kad butu 5 simboliai
+	public int CalculateThreeOfAKind(){
+		int payout = 0;
+		for(int row = 0; row < 5; row++){
+			for(int col = 0; col <= 2; col++){
+				Symbol a = Symbols[row, col];
+				Symbol b = Symbols[row, col +1];
+				Symbol c = Symbols[row, col +2];
+				if(a.GetType() == b.GetType() && b.GetType() == c.GetType()){
+					payout += GameConfig.ThreeOfAKindPayout;
+				}
 			}
+		}
+		GD.Print("Three of a kind payout: " + payout);
+		return payout;
+	}
+	public int CalculateSpinResult(){
 		
-		
-		
-		foreach(var texture in GridSlots){
-			Grid.AddChild(texture);
+		return 0;
+	}
+	
+	public void BuildGrid(){
+		for(int row = 0; row < 5; row++){
+			for(int col = 0; col < 5; col++){
+				var textureRect = new TextureRect();
+				textureRect.Texture = GD.Load<Texture2D>(Symbols[row, col].ImagePath);
+				GridSlots[row, col] = textureRect;
+				Grid.AddChild(textureRect);
+			}
 		}
 	}
-	public void UpdateGrid(int index, Symbol symbol){
-		
-		GridSlots[index].Texture = GD.Load<Texture2D>(symbol.ImagePath);
+	public void UpdateGrid(int row, int col, Symbol symbol){
+		GridSlots[row, col].Texture = GD.Load<Texture2D>(symbol.ImagePath);
 	}
 }
