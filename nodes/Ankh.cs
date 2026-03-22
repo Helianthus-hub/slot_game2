46f9eba88470545e29382b685b770648a5c57f70
antoxaMc <gray92929292@gmail.com>
2026-03-22 13:14:42 +0100

    Hand Calculation Files

--- nodes/Ankh.cs
+++ nodes/Ankh.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class Ankh : Symbol
 {
-    public override string ImagePath => "res://sprites/ankh.png";
+	public override string ImagePath => "res://sprites/ankh.png";
 	public override SymbolType Type => SymbolType.Ankh;
 	public Ankh(){}
 }
