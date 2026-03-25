2b63cbb600b2b6b9d7dd013063cb10c7eb5fc88c
benediktas.kairys@gmail.com <you@example.com>
2026-03-25 10:14:42 +0300

    Formating changes

--- nodes/GameConfig.cs
+++ nodes/GameConfig.cs
@@ -13,25 +13,25 @@ public static readonly int StartingAnkhAmount = 10;
 public static readonly int StartingHeartAmount = 10;
 
 //Hand payouts    
-public static readonly int ThreeOfAKindPayout = 100;
-public static readonly int FourOfAKindPayout = 200;
-public static readonly int FiveOfAKindPayout = 300;
-public static readonly int PyramidPayout = 500;
-public static readonly int FullHousePayout = 1000;
+public static int ThreeOfAKindPayout = 100;
+public static int FourOfAKindPayout = 200;
+public static int FiveOfAKindPayout = 300;
+public static int PyramidPayout = 500;
+public static int FullHousePayout = 1000;
 
-//Hand Probabilities
-public static readonly Dictionary<HandType, float> HandProbabilities = new (){
-        {HandType.HorizontalThree, 0.1f},
-        {HandType.HorizontalFour, 0.1f},
-        {HandType.HorizontalFive, 0.1f},
-        {HandType.DiagonalThree, 0.1f},
-        {HandType.DiagonalFour, 0.1f},
-        {HandType.DiagonalFive, 0.1f},
-        {HandType.ZigzagThree, 0.1f},
-        {HandType.ZigzagFour, 0.1f},
-        {HandType.ZigzagFive, 0.1f},
-        {HandType.PyramidUpper, 0.1f},
-        {HandType.PyramidLower, 0.1f},
-        {HandType.FullHouse, 0.1f}
-    };
+//Hand Probabilities will have to implement later
+public static Dictionary<HandType, float> HandProbabilities = new (){
+		{HandType.HorizontalThree, 0.1f},
+		{HandType.HorizontalFour, 0.1f},
+		{HandType.HorizontalFive, 0.1f},
+		{HandType.DiagonalThree, 0.1f},
+		{HandType.DiagonalFour, 0.1f},
+		{HandType.DiagonalFive, 0.1f},
+		{HandType.ZigzagThree, 0.1f},
+		{HandType.ZigzagFour, 0.1f},
+		{HandType.ZigzagFive, 0.1f},
+		{HandType.PyramidUpper, 0.1f},
+		{HandType.PyramidLower, 0.1f},
+		{HandType.FullHouse, 0.1f}
+	};
 }
--- nodes/SlotMachine/PayoutCalculator.cs
+++ nodes/SlotMachine/PayoutCalculator.cs
@@ -46,7 +46,156 @@ public static class PayoutCalculator {
 		}
 		return results;
 	}
+	
 	//Diagonal
+	public static List<HandResult> EvaluateDiagonal(Symbol[,] Symbols){
+		var results = new List<HandResult>();
+		//Startpoints
+		var StartPointsL = new List<Vector2I>() {new(2,0), new(1,0), new(0,0), new(0,1), new(0,2)};
+		var StartPointsR = new List<Vector2I>() {new(2,4), new(1,4), new(0,4), new(0,3), new(0,2)};
+		
+		//Definitions for the necessary components of the iterations
+		var head = Symbols[0,0];
+		var next = Symbols[1,1];
+		var nextPos = new Vector2I(1,1);
+		var headPos = new Vector2I(0,0);
+
+		var LtoR = new Vector2I(1, 1);
+		var RtoL = new Vector2I(1,-1);
+
+		
+
+		//Cheking necessary top left to right diagonals 
+		foreach(var startPoint in StartPointsL){
+			int MatchLength = 0;
+			headPos = startPoint;
+			head = Symbols[headPos.X, headPos.Y];
+			nextPos = startPoint + LtoR;
+			next = Symbols[nextPos.X ,nextPos.Y];
+			
+			while(nextPos.X < 5 || nextPos.Y < 5){
+					
+			
+				if(head.Type == next.Type){
+					if(nextPos.X == 4 || nextPos.Y == 4){
+						MatchLength++;
+						break;
+					}
+					nextPos += LtoR;
+					next = Symbols[nextPos.X, nextPos.Y];
+					MatchLength++;
+				
+				}
+				else if(MatchLength == 1){
+					MatchLength = 0;
+				}
+				else if (MatchLength > 1){
+					nextPos -= LtoR;
+					next = Symbols[nextPos.X, nextPos.Y];
+					break;
+				}
+				else{
+					headPos += LtoR;
+					//break contingency covered by the 2nd else statement 
+					nextPos = headPos + LtoR;
+					if(nextPos.X < 5 && nextPos.Y < 5){
+						head = Symbols[headPos.X, headPos.Y];
+						next = Symbols[nextPos.X, nextPos.Y];
+						
+					}
+					else break;
+				}
+				
+				
+			}
+			
+			if(MatchLength > 1){
+				(HandType type, int payout) = MatchLength switch {
+					4 => (HandType.DiagonalFive, GameConfig.FiveOfAKindPayout),
+					3 => (HandType.DiagonalFour, GameConfig.FourOfAKindPayout),
+					_ => (HandType.DiagonalThree, GameConfig.ThreeOfAKindPayout),
+				};
+				//Calculate Cells
+					var cells = new List<Vector2I>();
+					var MatchedSymbols = new List<Symbol>();
+					
+					while(MatchLength-- >= 0){
+					
+					cells.Add(new Vector2I(nextPos.X,nextPos.Y));
+					MatchedSymbols.Add(next);
+					nextPos -= LtoR;
+				}
+				//Calculate HandType
+					
+				results.Add(new HandResult(type, payout, cells, MatchedSymbols));
+			}
+		
+		
+	} 
+		foreach(var startPoint in StartPointsR){
+			int MatchLength = 0;
+			headPos = startPoint;
+			head = Symbols[headPos.X, headPos.Y];
+			nextPos = startPoint + RtoL;
+			next = Symbols[nextPos.X ,nextPos.Y];
+			
+			while(nextPos.X < 5 || nextPos.Y > -1){
+				if(head.Type == next.Type){
+					if(nextPos.X == 4 || nextPos.Y == 0){
+						MatchLength++;
+						break;
+					}
+					nextPos += RtoL;
+					next = Symbols[nextPos.X, nextPos.Y];
+					MatchLength++;
+				}
+				else if(MatchLength == 1){
+					MatchLength = 0;
+				}
+				else if (MatchLength > 1){
+					nextPos -= RtoL;
+					next = Symbols[nextPos.X, nextPos.Y];
+					break;
+				}
+				else{
+					headPos += RtoL;
+					//break contingency covered by the 2nd else statement 
+					nextPos = headPos + RtoL;
+					if(nextPos.X < 5 && nextPos.Y > -1){
+						head = Symbols[headPos.X, headPos.Y];
+						next = Symbols[nextPos.X, nextPos.Y];	
+					}
+					else break;
+				}
+				
+				
+			}
+			
+			if(MatchLength > 1){
+				(HandType type, int payout) = MatchLength switch {
+					4 => (HandType.DiagonalFive, GameConfig.FiveOfAKindPayout),
+					3 => (HandType.DiagonalFour, GameConfig.FourOfAKindPayout),
+					_ => (HandType.DiagonalThree, GameConfig.ThreeOfAKindPayout),
+				};
+				//Calculate Cells
+					var cells = new List<Vector2I>();
+					var MatchedSymbols = new List<Symbol>();
+					
+					while(MatchLength-- >= 0){
+					
+					cells.Add(new Vector2I(nextPos.X,nextPos.Y));
+					MatchedSymbols.Add(next);
+					nextPos -= RtoL;
+				}
+				//Calculate HandType
+					
+				results.Add(new HandResult(type, payout, cells, MatchedSymbols));
+			}
+		
+		
+	} 
+		return results;
+	}
 
 	//Pyramid
 	public static List<HandResult> EvaluatePyramid(Symbol[,] Symbols){
@@ -137,194 +286,6 @@ public static class PayoutCalculator {
 
 			results.Add(new HandResult(HandType.FullHouse, GameConfig.FullHousePayout,  cells, MatchedSymbols));
 		}
-		return results;
-	}
-	public static List<HandResult> EvaluateDiagonal(Symbol[,] Symbols)
-	{
-		var results = new List<HandResult>();
-		//Startpoints
-		var StartPointsL = new List<Vector2I>() {new(2,0), new(1,0), new(0,0), new(0,1), new(0,2)};
-		var StartPointsR = new List<Vector2I>() {new(2,4), new(1,4), new(0,4), new(0,3), new(0,2)};
-		//Generate Startpoints
-		// for(int i = 2; i > 0; i--)
-		// {
-		// 	StartPointsL.Add(new Vector2I(i,0));
-		// 	StartPointsL.Add(new Vector2I(0,i));
-		// }
-		// for(int i = 4; i > -1; i--)
-		// {
-			
-		// 	StartPointsR.Add(new Vector2I(4-i,4));
-		// 	StartPointsR.Add(new Vector2I(0,i));
-		// }
-
-		
-		//Definitions for the necessary components of the iterations
-		var head = Symbols[0,0];
-		var next = Symbols[1,1];
-		var nextPos = new Vector2I(1,1);
-		var headPos = new Vector2I(0,0);
-
-		var LtoR = new Vector2I(1, 1);
-		var RtoL = new Vector2I(1,-1);
-
-		
-
-		//Cheking necessary top left to right diagonals 
-		foreach(var startPoint in StartPointsL)
-		{
-			int MatchLength = 0;
-			headPos = startPoint;
-			head = Symbols[headPos.X, headPos.Y];
-			nextPos = startPoint + LtoR;
-			next = Symbols[nextPos.X ,nextPos.Y];
-			
-			while(nextPos.X < 5 || nextPos.Y < 5)
-			{
-					
-			
-				if(head.Type == next.Type)
-				{
-					
-					if(nextPos.X == 4 || nextPos.Y == 4)
-					{
-						MatchLength++;
-						break;
-					}
-					nextPos += LtoR;
-					next = Symbols[nextPos.X, nextPos.Y];
-					MatchLength++;
-				
-				}
-				else if(MatchLength == 1)
-				{
-					MatchLength = 0;
-				}
-				else if (MatchLength > 1)
-				{
-					nextPos -= LtoR;
-					next = Symbols[nextPos.X, nextPos.Y];
-					break;
-				}
-				else
-				{
-					headPos += LtoR;
-					//break contingency covered by the 2nd else statement 
-					nextPos = headPos + LtoR;
-					if(nextPos.X < 5 && nextPos.Y < 5)
-					{
-						head = Symbols[headPos.X, headPos.Y];
-						next = Symbols[nextPos.X, nextPos.Y];
-						
-					}
-					else break;
-				}
-				
-				
-			}
-			
-			if(MatchLength > 1)
-			{
-				(HandType type, int payout) = MatchLength switch {
-					4 => (HandType.DiagonalFive, GameConfig.FiveOfAKindPayout),
-					3 => (HandType.DiagonalFour, GameConfig.FourOfAKindPayout),
-					_ => (HandType.DiagonalThree, GameConfig.ThreeOfAKindPayout),
-				};
-				//Calculate Cells
-					var cells = new List<Vector2I>();
-					var MatchedSymbols = new List<Symbol>();
-					
-					while(MatchLength-- >= 0)
-				{
-					
-					cells.Add(new Vector2I(nextPos.X,nextPos.Y));
-					MatchedSymbols.Add(next);
-					nextPos -= LtoR;
-				}
-				//Calculate HandType
-					
-				results.Add(new HandResult(type, payout, cells, MatchedSymbols));
-			}
-		
-		
-	} 
-	foreach(var startPoint in StartPointsR)
-		{
-			int MatchLength = 0;
-			headPos = startPoint;
-			head = Symbols[headPos.X, headPos.Y];
-			nextPos = startPoint + RtoL;
-			next = Symbols[nextPos.X ,nextPos.Y];
-			
-			while(nextPos.X < 5 || nextPos.Y > -1)
-			{
-					
-			
-				if(head.Type == next.Type)
-				{
-					
-					if(nextPos.X == 4 || nextPos.Y == 0)
-					{
-						MatchLength++;
-						break;
-					}
-					nextPos += RtoL;
-					next = Symbols[nextPos.X, nextPos.Y];
-					MatchLength++;
-				
-				}
-				else if(MatchLength == 1)
-				{
-					MatchLength = 0;
-				}
-				else if (MatchLength > 1)
-				{
-					nextPos -= RtoL;
-					next = Symbols[nextPos.X, nextPos.Y];
-					break;
-				}
-				else
-				{
-					headPos += RtoL;
-					//break contingency covered by the 2nd else statement 
-					nextPos = headPos + RtoL;
-					if(nextPos.X < 5 && nextPos.Y > -1)
-					{
-						head = Symbols[headPos.X, headPos.Y];
-						next = Symbols[nextPos.X, nextPos.Y];
-						
-					}
-					else break;
-				}
-				
-				
-			}
-			
-			if(MatchLength > 1)
-			{
-				(HandType type, int payout) = MatchLength switch {
-					4 => (HandType.DiagonalFive, GameConfig.FiveOfAKindPayout),
-					3 => (HandType.DiagonalFour, GameConfig.FourOfAKindPayout),
-					_ => (HandType.DiagonalThree, GameConfig.ThreeOfAKindPayout),
-				};
-				//Calculate Cells
-					var cells = new List<Vector2I>();
-					var MatchedSymbols = new List<Symbol>();
-					
-					while(MatchLength-- >= 0)
-				{
-					
-					cells.Add(new Vector2I(nextPos.X,nextPos.Y));
-					MatchedSymbols.Add(next);
-					nextPos -= RtoL;
-				}
-				//Calculate HandType
-					
-				results.Add(new HandResult(type, payout, cells, MatchedSymbols));
-			}
-		
-		
-	} 
 		return results;
 	}
 }
