b6cd25764e087736a0de7d885e95a30347560956
Helianthus-hub <duckspot.yt@gmail.com>
2026-03-26 17:35:54 +0100

    nieko svarbaus

--- nodes/Modifiers/SymbolModifiers/SymbolModifiers.cs
+++ nodes/Modifiers/SymbolModifiers/SymbolModifiers.cs
@@ -4,4 +4,6 @@ using System;
 public abstract class SymbolModifiers{
     public abstract string Name {get;}
     //Implementuoti metodus kaip OnMatched() etc
+    public virtual void OnMatched(Symbol symbol, Symbol[,] grid){}
+    public virtual void OnSpinEnd(Symbol symbol, Symbol[,] grid) {}
 }
